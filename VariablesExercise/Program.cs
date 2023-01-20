14cbc49d198349752e1ddfea2a7b9f2562b2d49d
taylor Schuchter <taylorschuchter@gmail.com>
2023-01-20 16:35:11 -0500

    talked about my dog a lot

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+﻿{ 
+            string dogName = "Dirk";
+            int dogAge = 7;
+            char dogFootSize = 'M';
+            bool dogCute = true;
+            double dogVetBill = 323.15;
+            decimal dogWeight = 70.5m;
+
+            Console.WriteLine($"My dog's name is {dogName} and he is {dogAge} years old.");
+            Console.WriteLine($"He went to the vet, which cost ${dogVetBill}. They said he needed to lose weight because he is {dogWeight} pounds.");
+            Console.WriteLine($"The vet told us to get him shoes to protect his paws. We got size {dogFootSize}. Everyone took a poll and voted that" +
+                $" it's {dogCute} he looked cute in them");
         }
-    }
-}
+    
+
+
+
+
